5bad62eda6dd7c8fd83b1b5435f4a44d053bfa14
XavierTwitty <x.twitty@gmail.com>
2022-09-13 07:31:07 -0700

    clean up

--- index.js
+++ index.js
@@ -5,15 +5,12 @@ function showSlides() {
   var i;
   var slides = document.getElementsByClassName("mySlides");
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none"; 
+    slides[i].style.display = "none";
   }
   slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1} 
-  slides[slideIndex-1].style.display = "block"; 
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].style.display = "block";
   setTimeout(showSlides, 5000); // Change image every 2 seconds
 }
-
-console.log("hello world")
-
-  // components 21:13 
-  // react components 46:45 react dev tools chrome web store  
